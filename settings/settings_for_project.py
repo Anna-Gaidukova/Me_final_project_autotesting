bb11d7df7dea4dd276561626711bde659a3e5ad1
Anna-Gaidukova <anna.v.gaydukova@gmail.com>
2023-11-15 22:39:04 +0200

    changes

---
+++ settings/settings_for_project.py
@@ -0,0 +1,14 @@
+PROD_SERVER = "https://bookks.com.ua/"
+REG_URL = "https://bookks.com.ua/simpleregister/"
+TEST_EMAIL = "Absolutely_new.mail@gmail.com"
+PASSWORD = "Qwerty_1234"
+PASS_LOGIN = "Start38"
+FIRSTNAME = "ТестЮзер"
+LASTNAME = "ОнлиТест"
+PHONE = "+380500030303"
+CITY = "Киев"
+POSTCODE = "02001"
+ADDRESS = "30"
+IMPLICITLY_WAIT = 10
+EXPLICIT_WAIT = 10
+
